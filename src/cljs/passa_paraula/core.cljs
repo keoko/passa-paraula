ffe2de2057e0700daaaf41d1fb6ba3d255786100
natxo cabre <natxo.cabre@gmail.com>
2015-05-28 22:52:12 +0200

    moved navigation logic to the navigation namespace

--- src/cljs/passa_paraula/core.cljs
+++ src/cljs/passa_paraula/core.cljs
@@ -1,19 +1,7 @@
 (ns passa-paraula.core
-    (:require [reagent.core :as reagent]
-              [secretary.core :as secretary :include-macros true]
-              [passa-paraula.game :as game]
+    (:require [passa-paraula.game :as game]
               [passa-paraula.ui.views :as view]
-              [secretary.core :as secretary :include-macros true]
-              [reagent.session :as session]
-              [goog.events :as events]
-              [goog.history.EventType :as EventType])
-    (:import goog.History)) 
-
-
-
-
-(defn end-game []
-  (secretary/dispatch! "/end"))
+              [passa-paraula.navigation :as nav])) 
 
 
 (defn handle-letter [letter-id status]
@@ -22,7 +10,7 @@
     (game/update-score)
     (game/jump-next-letter)
     (when (game/end-game?)
-      (end-game))))
+      (nav/end-game))))
 
 (defn handle-keys [event]
   (when-let [key (.-charCode event)]
@@ -32,8 +20,7 @@
       107 (handle-letter (game/cur-letter-id) :failed)
       (.log js/console (str "key pressed not valid" key)))))
  
-(defn hook-keyboard-listener!
-  []
+(defn hook-keyboard-listener! []
    (.addEventListener js/window "keypress" handle-keys))
 
 
@@ -41,51 +28,15 @@
   (game/update-time)
   (js/setTimeout tick 1000)
   (when (game/end-game?)
-    (end-game)))
+    (nav/end-game)))
 
 (defn hook-clock-update! []
   (js/setTimeout tick 1000))
 
 
-(declare home-page about-page end-page)
-
-;; -------------------------
-;; Initialize app
-
-(secretary/set-config! :prefix "#")
-
-(defn current-page []
-  [:div [(session/get :current-page)]])
-
-(secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
-
-(secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
-
-(secretary/defroute "/end" []
-  (session/put! :current-page #'end-page))
-
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
-
-
-(defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
-
-
-
 (defn init! []
   (game/reset-state!)
-  (hook-browser-navigation!)
+  (nav/hook-browser-navigation!)
   (hook-keyboard-listener!)
   (hook-clock-update!)
-  (mount-root))
+  (nav/mount-root))
--- src/cljs/passa_paraula/navigation.cljs
+++ src/cljs/passa_paraula/navigation.cljs
@@ -1,12 +1,46 @@
 (ns passa-paraula.navigation
-  (:require [secretary.core :as secretary :include-macros true]
+  (:require [reagent.core :as reagent]
+            [secretary.core :as secretary :include-macros true]
+            [secretary.core :as secretary :include-macros true]
             [reagent.session :as session]
             [goog.events :as events]
-            [goog.history.EventType :as EventType])
+            [goog.history.EventType :as EventType]
+            [passa-paraula.ui.views :as view])
   (:import goog.History))
 
-(declare home-page about-page end-page)
 
+
+
+(defn end-game []
+  (secretary/dispatch! "/end"))
+
+
+(secretary/set-config! :prefix "#")
+
+(defn current-page []
+  [:div [(session/get :current-page)]])
+
+(secretary/defroute "/" []
+  (session/put! :current-page #'view/home-page))
+
+(secretary/defroute "/about" []
+  (session/put! :current-page #'view/about-page))
+
+(secretary/defroute "/end" []
+  (session/put! :current-page #'view/end-page))
+
+
+
+(defn mount-root []
+  (reagent/render [current-page] (.getElementById js/document "app")))
 
 ;; -------------------------
-;; Routes
+;; History
+;; must be called after routes have been defined
+(defn hook-browser-navigation! []
+  (doto (History.)
+    (events/listen
+     EventType/NAVIGATE
+     (fn [event]
+       (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
