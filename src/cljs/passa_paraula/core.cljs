dcd23bbe6afe59032f6cde6560c1852b0d08181a
natxo cabre <natxo.cabre@gmail.com>
2015-05-28 22:55:44 +0200

    moved events logic to events namespace

--- src/cljs/passa_paraula/core.cljs
+++ src/cljs/passa_paraula/core.cljs
@@ -1,42 +1,14 @@
 (ns passa-paraula.core
     (:require [passa-paraula.game :as game]
               [passa-paraula.ui.views :as view]
-              [passa-paraula.navigation :as nav])) 
+              [passa-paraula.navigation :as nav]
+              [passa-paraula.events :as events])) 
 
-
-(defn handle-letter [letter-id status]
-  (do
-    (game/change-letter-status letter-id status)
-    (game/update-score)
-    (game/jump-next-letter)
-    (when (game/end-game?)
-      (nav/end-game))))
-
-(defn handle-keys [event]
-  (when-let [key (.-charCode event)]
-    (case key
-      111 (handle-letter (game/cur-letter-id) :ok)
-      112 (handle-letter (game/cur-letter-id) :pass)
-      107 (handle-letter (game/cur-letter-id) :failed)
-      (.log js/console (str "key pressed not valid" key)))))
- 
-(defn hook-keyboard-listener! []
-   (.addEventListener js/window "keypress" handle-keys))
-
-
-(defn tick []
-  (game/update-time)
-  (js/setTimeout tick 1000)
-  (when (game/end-game?)
-    (nav/end-game)))
-
-(defn hook-clock-update! []
-  (js/setTimeout tick 1000))
 
 
 (defn init! []
   (game/reset-state!)
   (nav/hook-browser-navigation!)
-  (hook-keyboard-listener!)
-  (hook-clock-update!)
+  (events/hook-keyboard-listener!)
+  (events/hook-clock-update!)
   (nav/mount-root))
---
+++ src/cljs/passa_paraula/events.cljs
@@ -0,0 +1,34 @@
+(ns passa-paraula.events
+  (:require [passa-paraula.game :as game]
+            [passa-paraula.navigation :as nav]))
+
+
+(defn handle-letter [letter-id status]
+  (do
+    (game/change-letter-status letter-id status)
+    (game/update-score)
+    (game/jump-next-letter)
+    (when (game/end-game?)
+      (nav/end-game))))
+
+(defn handle-keys [event]
+  (when-let [key (.-charCode event)]
+    (case key
+      111 (handle-letter (game/cur-letter-id) :ok)
+      112 (handle-letter (game/cur-letter-id) :pass)
+      107 (handle-letter (game/cur-letter-id) :failed)
+      (.log js/console (str "key pressed not valid" key)))))
+ 
+(defn hook-keyboard-listener! []
+   (.addEventListener js/window "keypress" handle-keys))
+
+
+(defn tick []
+  (game/update-time)
+  (js/setTimeout tick 1000)
+  (when (game/end-game?)
+    (nav/end-game)))
+
+(defn hook-clock-update! []
+  (js/setTimeout tick 1000))
+
